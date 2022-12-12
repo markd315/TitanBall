9cf9c171b1b39c3409e4c8fd95ac7d620aecba42
markd315 <markd315@gmail.com>
2022-12-11 19:41:13 -0500

    try order instead of enablewebsecurity but this class is important suggestions to remove it are failing

--- src/authserver/SpringSecurityConfig.java
+++ src/authserver/SpringSecurityConfig.java
@@ -4,14 +4,15 @@ import authserver.jwt.JwtAuthenticationEntryPoint;
 import authserver.jwt.JwtAuthenticationFilter;
 import authserver.users.identities.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
+@Order(SecurityProperties.DEFAULT_FILTER_ORDER)
 @EnableGlobalMethodSecurity(
         securedEnabled = true,
         jsr250Enabled = true,
